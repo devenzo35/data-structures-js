d712c385bd16655f236b796c87f0a152d4b3cf64
Enzo <enzocuellar12@gmail.com>
2021-05-23 23:25:07 -0300

    Priority queue

--- queue.js
+++ queue.js
@@ -25,10 +25,61 @@ function Queue() {
 
 const queue = new Queue();
 
-queue.enqueue("a");
+/* queue.enqueue("a");
 queue.enqueue("b");
 queue.enqueue("c");
 queue.print();
 queue.dequeue();
 console.log(queue.front());
-queue.print();
+queue.print(); */
+
+function PriorityQueue() {
+  const collection = [];
+
+  this.print = () => {
+    console.log(collection);
+  };
+
+  this.enqueue = (element) => {
+    let added = false;
+
+    if (!collection.length) {
+      collection.push(element);
+    } else {
+      for (let i = 0; i < collection.length; i++) {
+        if (collection[i][1] < element[1]) {
+          collection.splice(i, 0, element);
+          added = true;
+          break;
+        }
+      }
+
+      if (!added) {
+        collection.push(element);
+      }
+    }
+  };
+  this.dequeue = () => {
+    collection.shift();
+  };
+  this.front = () => {
+    return collection[0];
+  };
+  this.size = () => {
+    return collection.length;
+  };
+  this.isEmpty = () => {
+    return collection.length === 0;
+  };
+}
+
+const priorityQueue = new PriorityQueue();
+
+priorityQueue.enqueue(["snacks", 1]);
+priorityQueue.enqueue(["food", 5]);
+priorityQueue.enqueue(["snacks", 1]);
+priorityQueue.enqueue(["drinks", 2]);
+priorityQueue.enqueue(["drinks", 2]);
+priorityQueue.enqueue(["drinks", 2]);
+priorityQueue.enqueue(["health", 6]);
+priorityQueue.print();
